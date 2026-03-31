ce58f04905399e5d9c6154237b8dd5c0cb49545f
Scott McCutchen <scott.mccutchen@soverance.com>
2026-03-31 17:59:14 -0400

    forum udpate

---
+++ src/Vanalytics.Data/Migrations/20260331215844_AddThreadSoftDelete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vanalytics.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThreadSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeletedBy",
+                table: "ForumThread",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ForumThread",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "ForumThread");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ForumThread");
+        }
+    }
+}
